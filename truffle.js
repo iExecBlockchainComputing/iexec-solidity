7e22934b323b8c8371a7cd52bd0f635067e9f2a9
Hadrien Croubois <hc@iex.ec>
2019-06-27 11:44:51 +0200

    adding build (0.0.11)

--- truffle.js
+++ truffle.js
@@ -1,3 +1,4 @@
+// var HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports =
 {
@@ -5,48 +6,47 @@ module.exports =
 	{
 		docker:
 		{
-			network_id: "*", // Match any network id,
 			host:       "iexec-geth-local",
 			port:       8545,
-			gas:        8000000,
-			gasPrice:   22000000000,
+			network_id: "*", // Match any network id,
+			gasPrice:   22000000000, //22Gwei
 		},
 		development:
 		{
-			network_id: "*", // Match any network id,
 			host:       "localhost",
 			port:       8545,
-			gas:        8000000,
-			gasPrice:   1000000000, //1Gwei
+			network_id: "*", // Match any network id,
+			gasPrice:   22000000000, //22Gwei
 		},
 		coverage:
 		{
+			host:       "localhost",
+			port:       8555,          // <-- If you change this, also set the port option in .solcover.js.
 			network_id: "*",
-			host:       "localhost",
-			port:       8555,            // <-- If you change this, also set the port option in .solcover.js.
 			gas:        0xFFFFFFFFFFF, // <-- Use this high gas value
-			gasPrice:   0x01        // <-- Use this low gas price
+			gasPrice:   0x01           // <-- Use this low gas price
 		},
-		ropsten:
-		{
-			network_id: 3,
-			host:       "localhost",
-			port:       8545,
-			gas:        8000000,
-			gasPrice:   4000000000, //4Gwei
-		},
-		kovan:
-		{
-			network_id: 42,
-			host:       "localhost",
-			port:       8545,
-			gas:        8000000,
-			gasPrice:   1000000000, //1Gwei
-		},
+		// mainnet:
+		// {
+		// 	provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.KOVAN_NODE),
+		// 	network_id: '1',
+		// 	gasPrice:   22000000000, //22Gwei
+		// },
+		// ropsten:
+		// {
+		// 	provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.KOVAN_NODE),
+		// 	network_id: '3',
+		// 	gasPrice:   22000000000, //22Gwei
+		// },
+		// kovan: {
+		// 	provider: () => new HDWalletProvider(process.env.DEPLOYER_MNEMONIC, process.env.KOVAN_NODE),
+		// 	network_id: '42',
+		// 	gasPrice:   10000000000, //10Gwei
+		// }
 	},
 	compilers: {
 		solc: {
-			version: "0.5.3",
+			version: "0.5.10",
 			settings: {
 				optimizer: {
 					enabled: true,

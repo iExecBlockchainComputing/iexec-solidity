a74542102a1c4969eca8fef0f947581f4f834a4c
Hadrien Croubois <hc@iex.ec>
2020-04-15 10:35:57 +0200

    debug erc1271/ERC1654

--- contracts/ERC1271/IERC1271.sol
+++ contracts/ERC1271/IERC1271.sol
@@ -1,30 +1,11 @@
 pragma solidity ^0.6.0;
 
-abstract contract IERC1271
+interface IERC1271
 {
-	// bytes4(keccak256("isValidSignature(bytes,bytes)")
-	bytes4 constant internal MAGICVALUE = 0x20c13b0b;
+	function isValidSignature(bytes calldata data, bytes calldata signature) external view returns (bytes4 magicValue);
+}
 
-	/**
-	 * @dev Should return whether the signature provided is valid for the provided data
-	 * @param _data Arbitrary length data signed on the behalf of address(this)
-	 * @param _signature Signature byte array associated with _data
-	 *
-	 * MUST return the bytes4 magic value 0x20c13b0b when function passes.
-	 * MUST NOT modify state (using STATICCALL for solc < 0.5, view modifier for solc > 0.5)
-	 * MUST allow external calls
-	 */
-	// function isValidSignature(
-	// 	bytes memory _data,
-	// 	bytes memory _signature)
-	// 	public
-	// 	view
-	// 	returns (bytes4 magicValue);
-
-	// Newer version ? From 0x V2
-	function isValidSignature(
-		bytes32 _data,
-		bytes memory _signature
-	)
-	public virtual view returns (bytes4);
+interface IERC1654
+{
+	function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bytes4 magicValue);
 }
